2c9d751d7c497fa835ce6943f667193fbe1749c4
mahanAhr <mhn.hr1383@gmail.com>
2025-11-28 12:06:07 +0330

    Added products page, profile page and providers setup

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
 export default function ProductsPage() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products"],
+    queryFn: () =>
+      fetch("https://fakestoreapi.com/products").then((res) => res.json()),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error loading products</p>;
+
   return (
     <div>
-      <h1>Product Page</h1>
+      <h1>Products</h1>
+
+      {data.map((product: any) => (
+        <div key={product.id}>
+          <p>{product.title}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
 export default function ProfilePage() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["user"],
+    queryFn: () =>
+      fetch("https://fakestoreapi.com/users/1").then((res) => res.json()),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Eror</p>;
+
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div style={{ padding: 20 }}>
+      <h1>User Profile</h1>
+
+      <p>Name:{data.name.firstname} {data.name.lastname}</p>
+      <p>User Name: {data.username}</p>
+      <p>E-mail:{data.email}</p>
+
+      <h3>Address:</h3>
+      <p>
+        {data.address.city} — {data.address.street}, {data.address.number}
+      </p>
     </div>
   );
 }
+
