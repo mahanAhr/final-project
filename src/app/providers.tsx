867fa0ab058240d4452d09e2610fb58c3de0e137
mahanAhr <mhn.hr1383@gmail.com>
2025-12-18 18:06:32 +0330

    Update after merge

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,29 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+
+  return (
+    <AppRouterCacheProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
+    </AppRouterCacheProvider>
+  );
 }
